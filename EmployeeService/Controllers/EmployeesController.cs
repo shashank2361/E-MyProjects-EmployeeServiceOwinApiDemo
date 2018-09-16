b6da03536a49e29277b3ea7335114b2e6d024b6a
DESKTOP-SB3UMDG\Shashank-PC <Shashank2361@yahoo.co.in>
2018-09-16 12:41:41 +0100

    First commit

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,141 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace EmployeeSerivce.Controllers
+{
+    //  [EnableCorsAttribute("*", "*", "*")]
+    [EnableCorsAttribute("http://localhost:51735", "*", "*")]
+    [RequireHttpsAttribute]
+    public class EmployeesController : ApiController
+    {
+         public HttpResponseMessage Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                  return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.ToList());
+             
+            }
+        }
+
+        [DisableCors]
+        public HttpResponseMessage Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "Employee with Id " + id.ToString() + " not found");
+                }
+            }
+        }
+
+        //public void Post([FromBody] Employee employee)
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        entities.Employees.Add(employee);
+        //        entities.SaveChanges();
+        //    }
+        //}
+
+
+        //to get 201 Item Created.With 201 status code we should also include the location i.e URI of the newly created item.To achieve this, change the implementation of the Post() method as shown below. 
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + "/" +   employee.ID.ToString());
+
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Employee with Id = " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+        
+public HttpResponseMessage Put(int id, [FromBody]Employee employee)
+{
+    try
+    {
+        using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        {
+            var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+            if (entity == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    "Employee with Id " + id.ToString() + " not found to update");
+            }
+            else
+            {
+                entity.FirstName = employee.FirstName;
+                entity.LastName = employee.LastName;
+                entity.Gender = employee.Gender;
+                entity.Salary = employee.Salary;
+
+                entities.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, entity);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+    }
+}
+
+
+    }
+}
